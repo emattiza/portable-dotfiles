87f4ffc9a41baa0eea67f202abb4a3797689e829
Evan Mattiza <emattiza@gmail.com>
2022-08-30 08:38:54 -0500

    use packages and devShell

--- flake.nix
+++ flake.nix
@@ -7,16 +7,20 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              curl
-              starship
-            ];
-            shellHook = ''
-              source <(${pkgs.starship}/bin/starship init bash --print-full-init)
-            '';
-          };
+        rec {
+	  packages = flake-utils.lib.flattenTree {
+		  myDevShell = pkgs.mkShell {
+		    buildInputs = with pkgs; [
+		      curl
+		      starship
+		    ];
+		    shellHook = ''
+		      source <(${pkgs.starship}/bin/starship init bash --print-full-init)
+		    '';
+		  };
+	  };
+	  defaultPackage = packages.myDevShell;
+	  devShell = packages.myDevShell;
         }
       );
 }
