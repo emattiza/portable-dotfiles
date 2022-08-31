dece431556660e82db9f544a0214a6c6dd92dc99
Evan Mattiza <emattiza@gmail.com>
2022-08-31 19:42:47 +0000

    add devShell file

--- flake.nix
+++ flake.nix
@@ -2,22 +2,14 @@
   description = "my portable dotfiles";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        rec {
-	  devShell = pkgs.mkShell {
-	    buildInputs = with pkgs; [
-	      curl
-	      starship
-	    ];
-	    shellHook = ''
-	      source <(${pkgs.starship}/bin/starship init bash --print-full-init)
-	    '';
-	  };
-        }
-      );
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      devShell = (import ./devShell.nix) {inherit pkgs;};
+    });
 }
-
