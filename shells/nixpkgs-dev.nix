2833f4d2e31e8d4296ad943b1f6965aea2d9dd93
emattiza <emattiza@gmail.com>
2023-08-11 10:56:01 -0500

    add git, remove starship

--- shells/nixpkgs-dev.nix
+++ shells/nixpkgs-dev.nix
@@ -2,16 +2,12 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     neovimEM
+    git
     hub
+    gh
     nixpkgs-update
     nixpkgs-review
     nix-output-monitor
     ranger
   ];
-  shellHook = ''
-    if [[ ! $HOST_PATH == *"starship"* ]];
-    then
-       source <(starship init bash --print-full-init)
-    fi
-  '';
 }
